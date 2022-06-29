591f4a290572c0e6428bf4eae7f850475db73d6d
KenyaMasuko <ken12rug@gmail.com>
2022-06-29 14:09:47 +0900

    webpack-merge version upgraded

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,22 @@
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
-const webpackMerge = require('webpack-merge');
-const commonConf = require('./webpack.common');
-const outputFile = '[name].[chunkhash]';
-const assetFile = '[contenthash]';
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
+const { merge } = require("webpack-merge");
+const commonConf = require("./webpack.common");
+const outputFile = "[name].[chunkhash]";
+const assetFile = "[contenthash]";
 const htmlMinifyOption = {
-    collapseWhitespace: true,
-    removeComments: true,
-    removeRedundantAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    useShortDoctype: true
+	collapseWhitespace: true,
+	removeComments: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	useShortDoctype: true,
 };
 
-module.exports = () => webpackMerge(commonConf({ outputFile, assetFile, htmlMinifyOption }), {
-    mode: 'production',
-    optimization: {
-        minimizer: [
-            new TerserPlugin(),
-            new OptimizeCssPlugin()
-        ],
-    }
-});
+module.exports = () =>
+	merge(commonConf({ outputFile, assetFile, htmlMinifyOption }), {
+		mode: "production",
+		optimization: {
+			minimizer: [new TerserPlugin(), new OptimizeCssPlugin()],
+		},
+	});
